666d9011667dd819746202cda93440023ce055ea
Shelly Foxworth-Hill <sfhill24@gmail.com>
2022-10-10 14:01:02 -0400

    updated PORT call

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const {animals} = require('./data/animals');
 
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -41,6 +42,12 @@ function filterByQuery(query, animalsArray) {
     // return the filtered results:
     return filteredResults;
   }
+
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+
   app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -49,6 +56,15 @@ function filterByQuery(query, animalsArray) {
     res.json(results);
   });
 
-app.listen(3001, () => {
-    console.log(`API server now port 3001!`);
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
 });
+
+  app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
